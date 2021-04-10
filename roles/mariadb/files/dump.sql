2dc303ad9f44b267895779c83a0c2bbc6003f4a3
Kalpesh Chaudhari <13450744+thekalpesh7@users.noreply.github.com>
2021-04-10 20:51:46 +0530

    Updated index.html

--- roles/mariadb/files/dump.sql
+++ roles/mariadb/files/dump.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS demo (
   message varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO demo (message) VALUES('O2N Lamp Stack!');
+INSERT INTO demo (message) VALUES('O2N Lamp Stack! This msg is from Database!');
